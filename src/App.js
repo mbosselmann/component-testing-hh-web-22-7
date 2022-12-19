2acbe58965672e3f40f39f67445a237478ffb6d4
Mareike Bosselmann <mareike.bosselmann@gmx.de>
2022-12-19 14:54:18 +0100

    Update app structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import styled from "styled-components";
-import HomePage from "./pages/HomePage.js";
+import Movies from "./components/Movies/Movies.js";
+
+const initialMovies = [
+  {
+    id: "28djdh72",
+    name: "The Incredible Hulk",
+    isLiked: false,
+  },
+  {
+    id: "dknseu2",
+    name: "Spiderman 1-25",
+    isLiked: false,
+  },
+  {
+    id: "dkwi02ksk",
+    name: "Lord of the Rings",
+    isLiked: true,
+  },
+];
 
 const StyledApp = styled.div`
   width: min(500px, 100% - 2rem);
@@ -8,7 +26,7 @@ const StyledApp = styled.div`
 export default function App() {
   return (
     <StyledApp>
-      <HomePage />
+      <Movies initialMovies={initialMovies} />
     </StyledApp>
   );
 }
